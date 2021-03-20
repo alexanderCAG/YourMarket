c18a9c21d1faf5188d8ae9c2baef62d1a87a71d8
Alexander <alexanderabbey98@gmail.com>
2021-03-20 02:04:15 +0000

    update

--- Front/base.php
+++ Front/base.php
@@ -4,30 +4,6 @@
     require "head.php";
 ?>
 <!-- Mon code -->
-
-<div class="scroll-bg">
-    <div class="scroll-div">
-        <div class="row scroll-object">
-            <div class="col-6">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, aliquam ipsum doloribus facilis labore aspernatur, distinctio repellendus saepe accusantium incidunt numquam. Alias vero excepturi ipsam in, ullam quis molestias rerum.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit minima temporibus a consequatur sunt repellat ullam delectus ut excepturi aliquid? Repudiandae nam hic repellendus quis magni similique nobis natus nemo.
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem architecto, facere ea, expedita blanditiis quasi modi molestiae unde accusamus aperiam, perspiciatis in minima maiores vitae doloribus? Voluptates magni tempora praesentium!
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti dolore vero quibusdam numquam nemo eligendi doloremque explicabo perspiciatis consectetur nostrum! Et magni vero distinctio quod reprehenderit quo eveniet harum vel!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi et dolor sint natus, exercitationem ipsam odit harum quis, ad incidunt consequuntur debitis vitae perferendis reiciendis odio aliquid totam alias dolorem?
-            </div>
-
-            <div class="col-6">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, aliquam ipsum doloribus facilis labore aspernatur, distinctio repellendus saepe accusantium incidunt numquam. Alias vero excepturi ipsam in, ullam quis molestias rerum.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit minima temporibus a consequatur sunt repellat ullam delectus ut excepturi aliquid? Repudiandae nam hic repellendus quis magni similique nobis natus nemo.
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem architecto, facere ea, expedita blanditiis quasi modi molestiae unde accusamus aperiam, perspiciatis in minima maiores vitae doloribus? Voluptates magni tempora praesentium!
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti dolore vero quibusdam numquam nemo eligendi doloremque explicabo perspiciatis consectetur nostrum! Et magni vero distinctio quod reprehenderit quo eveniet harum vel!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi et dolor sint natus, exercitationem ipsam odit harum quis, ad incidunt consequuntur debitis vitae perferendis reiciendis odio aliquid totam alias dolorem?
-            </div>
-            
-        </div>
-    </div>
-</div>
-
 <br><a href="index.php">ICI</a>
 <?php
     include("footer.php");
