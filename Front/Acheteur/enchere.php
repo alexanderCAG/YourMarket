17f9a0db5925fd2cf994b937ff437ba64e6383cd
Alexander <alexanderabbey98@gmail.com>
2021-04-03 22:57:05 +0100

    enchere base

--- Front/Acheteur/enchere.php
+++ Front/Acheteur/enchere.php
@@ -30,9 +30,7 @@ for ($i=0; $i<=$totalEnchere;$i++){
                 if($rowAllEnchere = mysqli_fetch_assoc($queryAllEnchere)){
                     $nameEnchere = $rowAllEnchere['name'];
                     $prixEnchere = $rowAllEnchere['price'];
-                    // $brandEnchere = $rowAllEnchere['brand'];
                     $quantityEnchere = $rowAllEnchere['quantity'];
-                    // $end_enchere = $rowAllEnchere['end_enchere'];
                     $descriptionEnchere = $rowAllEnchere['description'];
                     $photo = $rowAllEnchere['photo'];
 
@@ -52,58 +50,9 @@ for ($i=0; $i<=$totalEnchere;$i++){
 
             <div class="col-6 enchere_liste_details position-relative">
                 <div class="enchere_info_titre texte_style text-uppercase"><?php echo $nameEnchere ?></div>
-                <!-- <div class="enchere_info_marque detail_style text-uppercase"><?php echo $brandEnchere ?></div> -->
                 <br>
                 <div class="enchere_info_description"><?php echo $descriptionEnchere ?></div>
-                <!-- <div><?php echo $end_enchere ?></div> -->
-                <!-- <script type="text/javascript">
-                        
 
-                        alert(monthEndBid2);
-                        var date2 =  dateBDD;//year, month, day, hour, minute, second, and millisecond
-                        var date2 = new Date(yearEndBid, monthEndBid, dayEndBid, hourEndBid, minuteEndBid, secondEndBid, millisecondEndBid);//year, month, day, hour, minute, second, and millisecond
-                        var dateBDD ='<?php echo $end_enchere ?>'
-
-                        var yearEndBid = dateBDD[0]+dateBDD[1]+dateBDD[2]+dateBDD[3]; 
-                        var monthEndBid = dateBDD[5]+dateBDD[6]; 
-                        var dayEndBid = dateBDD[8]+dateBDD[9]; 
-                        var hourEndBid = dateBDD[11]+dateBDD[12]; 
-                        var minuteEndBid = dateBDD[14]+dateBDD[15]; 
-                        var secondEndBid = dateBDD[17]+dateBDD[18]; 
-                        var millisecondEndBid = 0; 
-
-                        var date2 = new Date(yearEndBid, monthEndBid, dayEndBid, hourEndBid, minuteEndBid, secondEndBid, millisecondEndBid);
-    
-                        
-                </script> -->
-                <!-- <div class="enchere_compteur_total centrer row">
-                    <div class="col-2 div_timeur_compteur ">
-                        <img class="img_timeur_compteur" src="../../Image/timeur_compteur.png" alt="timeur_compteur">
-                    </div>
-                    <div class="col-9">
-                        <div class="row" id="countdown" data-time="<?php echo $end_enchere ?>">
-                            <div class="enchere_compteur col-3" >
-                                <font size=6><strong id="jours"></strong></font>
-                                <em>Jours</em>
-                            </div>
-                            <div class="enchere_compteur col-3" >
-                                <font size=6><strong id="heures"></strong></font>
-                                <em>Houres</em>
-                            </div>
-                            <div class="enchere_compteur col-3" >
-                                <font size=6><strong id="minutes"></strong></font>
-                                <em>Minutes</em>
-                            </div>
-                            <div class="enchere_compteur col-3" >
-                                <font size=6><strong id="secondes"></strong></font>
-                                <em>Secondes</em>
-                            </div>
-                            <div class="col-3"></div>
-                        </div>
-                        
-                    </div>
-                </div> -->
-            
             <div class="col-1"></div>
         </div>
 
@@ -112,18 +61,9 @@ for ($i=0; $i<=$totalEnchere;$i++){
 
     </div>
 
-    <!-- <div class="col-3 enchere_liste_payer centrer position-relative">
-        <div class="enchere_payer_titre texte_style text-uppercase mt-4">faire une offre</div>
-        <input class="enchere_propose_prix centrer" placeholder="Proposition (€)">
-        <button class="btn_envoyer_propose_prix text-uppercase">Envoyer</button>
-        <div class="enchere_nombre_participant detail_style position-absolute bottom-0 end-0">135 participants</div>
-    </div> -->
-
     <div class="col-3 enchere_liste_payer centrer position-relative">
         <div class="enchere_payer_titre texte_style text-uppercase ">interested ?</div>
-        <!-- <input class="enchere_propose_prix centrer" placeholder="Proposition (€)"> -->
         <a type="button" href="enchere_unique.php?iditem=<?= $id_item?>" class="btn_envoyer_propose_prix text-uppercase">View more</a>
-        <!-- <div class="enchere_nombre_participant detail_style position-absolute bottom-0 end-0">135 participants</div> -->
     </div>
 
     <div class="col-1"></div>
--- Front/Base/enchere_accueil.php
+++ Front/Base/enchere_accueil.php
@@ -3,14 +3,9 @@
     $title="Page enchère";
     require "head.php";
         include("../../Bdd/cnx.php");
-        
+        $queryIdItem = mysqli_query($con, "select id_item from item where is_bidding=1"); 
         $queryCountEnchere = mysqli_query($con, "select count(id_item) as total  from item where is_bidding=1"); 
-        $queryAllEnchere = mysqli_query($con, "select name, end_enchere, price ,subcategory, brand, quantity, description,photo, is_negotiated, is_bidding 
-        from item,bid,seller
-        where bid.id_item=item.id_item
-        and bid.id_seller=seller.id_seller
-        and is_bidding=1
-        ");
+        $queryAllEnchere = mysqli_query($con, "select name, end_enchere, price ,subcategory, quantity, description,photo, is_bidding from item where is_bidding=1");
 ?>
  <?php
  if($row = mysqli_fetch_assoc($queryCountEnchere)){
@@ -29,17 +24,18 @@
 
     <div class="class_separation position-absolute bottom-0 start-50 translate-middle-x"></div>
 </div>
+
 <?php
 for ($i=0; $i<=$totalEnchere;$i++){
                 if($rowAllEnchere = mysqli_fetch_assoc($queryAllEnchere)){
                     $nameEnchere = $rowAllEnchere['name'];
                     $prixEnchere = $rowAllEnchere['price'];
-                    $brandEnchere = $rowAllEnchere['brand'];
                     $quantityEnchere = $rowAllEnchere['quantity'];
-                    $end_enchere = $rowAllEnchere['end_enchere'];
                     $descriptionEnchere = $rowAllEnchere['description'];
-                    $is_negotiated = $rowAllEnchere['is_negotiated'];
                     $photo = $rowAllEnchere['photo'];
+
+                    if($rowItem = mysqli_fetch_assoc($queryIdItem)){
+                        $id_item = $rowItem['id_item'];
                     ?>
                     
 <div class="enchere_liste_total row">
@@ -54,38 +50,9 @@ for ($i=0; $i<=$totalEnchere;$i++){
 
             <div class="col-6 enchere_liste_details position-relative">
                 <div class="enchere_info_titre texte_style text-uppercase"><?php echo $nameEnchere ?></div>
-                <div class="enchere_info_marque detail_style text-uppercase"><?php echo $brandEnchere ?></div>
                 <br>
                 <div class="enchere_info_description"><?php echo $descriptionEnchere ?></div>
 
-                <div class="enchere_compteur_total centrer row">
-                    <div class="col-2 div_timeur_compteur ">
-                        <img class="img_timeur_compteur" src="../../Image/timeur_compteur.png" alt="timeur_compteur">
-                    </div>
-                    <div class="col-9">
-                        <div class="row" id="countdown" data-time="<?php echo $end_enchere ?>">
-                            <div class="enchere_compteur col-3" >
-                                <font size=6><strong id="jours"></strong></font>
-                                <em>Jours</em>
-                            </div>
-                            <div class="enchere_compteur col-3" >
-                                <font size=6><strong id="heures"></strong></font>
-                                <em>Houres</em>
-                            </div>
-                            <div class="enchere_compteur col-3" >
-                                <font size=6><strong id="minutes"></strong></font>
-                                <em>Minutes</em>
-                            </div>
-                            <div class="enchere_compteur col-3" >
-                                <font size=6><strong id="secondes"></strong></font>
-                                <em>Secondes</em>
-                            </div>
-                            <div class="col-3"></div>
-                        </div>
-                        
-                    </div>
-                </div>
-            
             <div class="col-1"></div>
         </div>
 
@@ -95,16 +62,16 @@ for ($i=0; $i<=$totalEnchere;$i++){
     </div>
 
     <div class="col-3 enchere_liste_payer centrer position-relative">
-        <div class="enchere_payer_titre texte_style text-uppercase mt-4">faire une offre</div>
-        <input class="enchere_propose_prix centrer" placeholder="Proposition (€)">
-        <a type="button" class="btn_envoyer_propose_prix text-uppercase" href="connexion.php" style="font-weight:bold;padding-bottom:20px!important;text-decoration:none;color:white;">Envoyer</a>
-        <div class="enchere_nombre_participant detail_style position-absolute bottom-0 end-0">135 participants</div>
+        <div class="enchere_payer_titre texte_style text-uppercase ">interested ?</div>
+        <a type="button" href="connexion.php" class="btn_envoyer_propose_prix text-uppercase">View more</a>
     </div>
 
     <div class="col-1"></div>
 
 </div>
+
 <?php
+                    }
                 }
             }
         }
