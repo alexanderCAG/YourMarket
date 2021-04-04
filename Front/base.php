7713feafd1911132193900bf5b5a621d1a7b602b
Alexander <alexanderabbey98@gmail.com>
2021-04-04 21:51:16 +0100

    Update base.php

--- Front/base.php
+++ Front/base.php
@@ -4,13 +4,6 @@
     require "head.php";
 ?>
 
-<div class="mt-5">
-    <button class="fas fa-star"></button>
-    <button class="fas fa-star"></button>
-    <button class="fas fa-star"></button>
-    <button class="fas fa-star"></button>
-    <button class="fas fa-star"></button>
-</div>
 
 <!-- Mon code -->
 <br><a href="index.php">ICI</a>
