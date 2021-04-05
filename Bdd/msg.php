93cbcd3b4609d42d2ae61c320b9164725678d21e
Alexander <alexanderabbey98@gmail.com>
2021-04-05 19:07:01 +0100

    Update msg.php

--- Bdd/msg.php
+++ Bdd/msg.php
@@ -1,26 +1,5 @@
 
 
-<!-- <?php
-        
-    include("cnx.php");
-    session_start();
-    $idOffre = $_SESSION['id_offer'];
-    if(isset($_POST['submit_msg_acheteur'])){
-        $quantite_nego_message = htmlspecialchars($_POST['quantite_nego_message']);
-        $prix_nego_message = nl2br(htmlspecialchars($_POST['prix_nego_message']));
-        $nb_nego = $_POST['nb_nego'];
-        $id_offer = $_POST['id_offer'];
-        $nb_nego=$nb_nego+1;
-    
-        if($nb_nego<=5){
-            // $query = mysqli_query($con, "UPDATE offer SET price_offered='$prix_nego_message',quantity='$quantite_nego_message',nb_nego='$nb_nego',status='en cours' WHERE id_offer='$id_offer'");
-        }
-        echo $idOffre;
-        // echo "<script language='javascript' type='text/javascript'> location.href='../Front/Acheteur/messagerie.php' </script>";
-    }
-        
-?> -->
-
 
 <!-- loading function for intant message -->
 <?php
